b20051168f9d9435929d05b5f181b4345969c896
Maksym Szemer <115214461+MaksymSz@users.noreply.github.com>
2024-01-15 19:47:38 +0100

    Update summary.js

--- src/components/sites/summary.js
+++ src/components/sites/summary.js
@@ -12,18 +12,18 @@ import "./summary.css";
  */
 function Summary({avgUsers, avgTime, newUsers, users}) {
     return (
-        <div className="summary-container">
+        <div>
             <center>
                 <h2>Podsumowanie danych:</h2>
                 <div>
-                    <span className="constant-tilt-shake">Średnia ilość osób dziennie: {avgUsers}</span>
-                    <span className="constant-tilt-shake">Średni czas spędzany na siłowni: {avgTime}</span>
+                    <span>Średnia ilość osób dziennie: {avgUsers}</span>
+                    <span>Średni czas spędzany na siłowni: {avgTime}</span>
                 </div>
                 <br/>
                 <br/>
                 <div>
-                    <span className="constant-tilt-shake">Liczba nowych klientów: {newUsers}</span>
-                    <span className="constant-tilt-shake">Łączna liczba klientów: {users}</span>
+                    <span>Liczba nowych klientów: {newUsers}</span>
+                    <span>Łączna liczba klientów: {users}</span>
                 </div>
             </center>
         </div>
